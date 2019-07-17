124f0ce6b6b5a30c4c5a2032f5ba6fd701742355
Nick Schoch <nschoch@gmail.com>
2019-07-17 08:47:18 -0700

    fix extra .signed in remember token cookie storage

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,15 @@
 module SessionsHelper
   def log_in(user)
-    session[:user_id] = user.id  
+    session[:user_id] = user.id
   end
 
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent.signed[:remember_token] = user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
-    debugger
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
