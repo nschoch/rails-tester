129ea73ecc7956ed7801b829c7480c0bffabafd9
Nick Schoch <nschoch@gmail.com>
2019-07-22 14:14:18 -0700

    after one crash

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   post   '/signup',   to: 'users#create'
   get    '/login',    to: 'sessions#new'
   post   '/login',    to: 'sessions#create'
-  delete 'logout',    to: 'sessions#destroy'
+  delete '/logout',    to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
